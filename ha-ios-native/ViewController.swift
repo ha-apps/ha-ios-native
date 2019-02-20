ece0cbb0091d3edf9e6556ec6b288d2add8d633f
Evgenii Khramkov <evkhramk@microsoft.com>
2019-02-20 10:40:51 +0300

    Initial Commit

---
+++ ha-ios-native/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ha-ios-native
+//
+//  Created by Evgenii Khramkov on 2/20/19.
+//  Copyright © 2019 ewgeniux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
