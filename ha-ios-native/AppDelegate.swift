7029c7a28a9164c412597fe38e2ea9e23b9a28d5
Evgenii Khramkov <evkhramk@microsoft.com>
2019-02-25 16:23:55 +0300

    integrate HA SDK

--- ha-ios-native/AppDelegate.swift
+++ ha-ios-native/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import HockeySDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        BITHockeyManager.shared().configure(withIdentifier: "APP_IDENTIFIER")
+        BITHockeyManager.shared().start()
+        BITHockeyManager.shared().authenticator.authenticateInstallation()
+        // BITHockeyManager.shared().crashManager.generateTestCrash()
         return true
     }
 
